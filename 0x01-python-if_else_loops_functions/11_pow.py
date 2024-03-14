2ddb33f2328217c0d440eff83147ea0d7953596a
Mikonimo <mikonani413@gmail.com>
2024-03-14 06:51:10 +0300

    update

---
+++ 0x01-python-if_else_loops_functions/11_pow.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """
+    Computes a to the power of b and returns the result.
+    Args:
+        a: The base.
+        b: The exponent.
+    Returns:
+        The value of a raised to the power of b.
+    """
+    return a ** b

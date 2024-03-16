0a144d6ebb7615369332b690e7e37e30524ebb60
Mikonimo <mikonani413@gmail.com>
2024-03-16 15:48:52 +0300

    Task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -9,7 +9,7 @@ def replace_in_list(my_list, idx, element):
     Returns:
     the new modified list
     original list if idx is negative or if idx is out of range
-    """ 
+    """
     if idx < 0 or idx >= len(my_list):
         return (my_list[:])
     else:

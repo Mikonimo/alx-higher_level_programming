f0a3eff670802175d7edbaf1552129160abfa39d
Mikonimo <mikonani413@gmail.com>
2024-03-16 15:16:57 +0300

    Task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """
+    Retrieves an element from a list like in C
+    Args:
+    my_list: the list being evaluated
+    idx: the index of the element in the list
+    Returns:
+    the element retrieved
+    none if idx is negative or it is out of range
+    """
+    if idx < 0 or idx > len(my_list):
+        return None
+    return my_list([idx])

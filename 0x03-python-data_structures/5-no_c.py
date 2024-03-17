dff0283a7d86f284ea4cc66eaffa18ce64ab273c
Mikonimo <mikonani413@gmail.com>
2024-03-17 10:12:40 +0300

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -8,7 +8,7 @@ def no_c(my_string):
     the new string
     """
     new_string = ''
-    for ch in my_string[:]:   
+    for ch in my_string[:]:
         if ch != 'c' and ch != 'C':
             new_string += ch
     return (new_string)

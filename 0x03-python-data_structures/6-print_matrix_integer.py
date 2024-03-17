714cc97d8bbeb3cfba04682699edc0346a27c18a
Mikonimo <mikonani413@gmail.com>
2024-03-17 10:40:59 +0300

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """
+    Prints a matrix of integers.
+    Args:
+    matrix: the matrix of integers
+    """
+    for r in matrix:
+        for c in r:
+            if c != 0:
+                print(" ", end='')
+            print("{:d}".format(c), end='')
+        print()

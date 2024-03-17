d212e8aec55ee5f777e2b934c20c199ea5c3972c
Mikonimo <mikonani413@gmail.com>
2024-03-17 13:07:36 +0300

    TAsk 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """
+    Finds all multiples of 2 in a list
+    Args:
+    my_list: the list being evaluated
+    Return:
+    a new list with True of False depending on whether
+    the integer at the same position in the original
+    list is a multiple of 2
+    """
+    d_list = []
+
+    for n in range(len(my_list)):
+        if my_list[n] % 2 == 0:
+            d_list.append(True)
+        else:
+            d_list.append(False)
+    
+    return (d_list)

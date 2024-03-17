0de1c25bc78d8a89c6abc1f03408d93a15ee1d48
Mikonimo <mikonani413@gmail.com>
2024-03-17 10:10:47 +0300

    Task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """
+    Removes all characters c and C from a string
+    Args:
+    my_string: the string being evaluated
+    Returns:
+    the new string
+    """
+    new_string = ''
+    for ch in my_string[:]:     
+        if ch != 'c' and ch != 'C':
+            new_string += ch
+    return (new_string)

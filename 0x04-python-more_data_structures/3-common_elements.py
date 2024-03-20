657e0fccc7df6bf5680188eb548cd03756875f11
Mikonimo <mikonani413@gmail.com>
2024-03-20 17:11:25 +0300

    Task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """
+    Returns a set of common elements in two sets
+    Args:
+    set_1: the first set
+    set_2: the second set
+    Return:
+    set of common elements from set_1 and set_2
+    """
+    com_set = set_1 & set_2
+    return(com_set)

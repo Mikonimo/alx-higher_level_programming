10b0ec746dd184a8c2cc0b9a1a5aee98e9bee76d
Mikonimo <mikonani413@gmail.com>
2024-03-20 17:28:17 +0300

    Task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary
+    Args:
+    a_dictionary: the dictionary being evaluated
+    Return:
+    number of keys in the a_dictionary
+    """
+    return (len(a_dictionary))

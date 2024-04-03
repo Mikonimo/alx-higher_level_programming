49d77f4c03791221a8b713ce086968676512a0ba
Mikonimo <mikonani413@gmail.com>
2024-04-04 02:48:46 +0300

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Initialize a new square
+        
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size

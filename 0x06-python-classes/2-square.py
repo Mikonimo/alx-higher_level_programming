512c45e2270d4413e69b7dd8e1181be3d91fa05e
Mikonimo <mikonani413@gmail.com>
2024-04-04 02:55:36 +0300

    Tasl 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,10 +5,14 @@
 class Square:
     """Defines a square"""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize a new square
 
         Args:
             size (int): The size of the new square.
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size

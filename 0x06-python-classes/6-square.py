b9be1ca828bee6deb45a11d2fc52f22280aaa834
Mikonimo <mikonani413@gmail.com>
2024-04-04 03:40:41 +0300

    Task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve the current size of the square"""
+        return self.__size
+
+    @property
+    def position(self):
+        """Retrieve the current positon of the square"""
+        return (self.__position)
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square
+        Args:
+            value: the size of the square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Return the current square area
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Print the square with the # char."""
+        if self.__size == 0:
+            print("")
+            return
+        else:
+            [print("") for i in range(0, self.__position[1])]
+            for i in range(0, self.__size):
+                [print(" ", end="") for k in range(0, self.__position[0])]
+                [print("#", end="") for j in range(self.__size)]
+                print("")

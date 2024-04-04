f40382afda9047c7a244c3ff9a368e298df2f07b
Mikonimo <mikonani413@gmail.com>
2024-04-04 03:52:37 +0300

    Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -23,6 +23,15 @@ class Square:
         """Retrieve the current positon of the square"""
         return (self.__position)
 
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     @size.setter
     def size(self, value):
         """Set the size of the square

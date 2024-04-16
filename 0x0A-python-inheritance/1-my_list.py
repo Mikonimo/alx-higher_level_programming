4bf1ea824c51bbf106a4ac92940d588ff44fb38c
Mikonimo <mikonani413@gmail.com>
2024-04-16 04:03:22 +0300

    Task 2

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,8 +4,6 @@
 
 class MyList(list):
     """My list class"""
-
     def print_sorted(self):
         """Prints the list in ascending sort"""
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))

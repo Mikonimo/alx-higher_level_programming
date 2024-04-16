c8ce426a78b9709752b4f121e0a2439a2fe337a1
Mikonimo <mikonani413@gmail.com>
2024-04-16 04:19:59 +0300

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module contains the BaseGeometry class"""
+
+
+class BaseGeometry:
+    """The class BaseGeometry"""
+    def area(self):
+        """area function"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates the value is an integer
+        Args:
+            @name: always a string
+            @value: must be an integer
+        """
+        if (not isinstance(value, int)):
+            raise TypeError("<name> must be an integer")
+        if(value <= 0):
+            raise ValueError("<name> must be greater than 0")

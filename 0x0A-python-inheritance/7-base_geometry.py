42c35c720c407c6bceb883e7063edf6167025bcd
Mikonimo <mikonani413@gmail.com>
2024-04-16 04:52:53 +0300

    Task 8

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -17,5 +17,5 @@ class BaseGeometry:
         """
         if (not isinstance(value, int)):
             raise TypeError("<name> must be an integer")
-        if(value <= 0):
+        if (value <= 0):
             raise ValueError("<name> must be greater than 0")
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """Inherits from class BaseGeometry"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Class Rectangle"""
     def __init__(self, width, height):
@@ -11,7 +12,7 @@ class Rectangle(BaseGeometry):
             width: the width of the rectangle
             height: the height of the rectangle
         """
-        self.integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        self.integer_validator("width", width)
         self.__height = height
+        self.integer_validator("height", height)

42970a4ff66fc3850ff05ce6372e7f8265ac7af3
Mikonimo <mikonani413@gmail.com>
2024-04-16 05:56:58 +0300

    adv

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class MyInt(int):
+    def __eq__(self, other):
+        """
+        Overrides the equality operator.
+        Inverts the behavior of the equality operator.
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Overrides the inequality operator.
+        Inverts the behavior of the inequality operator.
+        """
+        return super().__eq__(other)

9f72b0a2f39c515a178fd78273e42e7d3b6d3bb9
Mikonimo <mikonani413@gmail.com>
2024-04-16 04:09:33 +0300

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module contains the BaseGeometry class"""
+
+
+class BaseGeometry:
+    """The class BaseGeometry"""
+    def area(self):
+        """area function"""
+        raise("area() is not implemented")

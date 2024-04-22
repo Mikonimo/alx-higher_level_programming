8b26eec1970d4cb890c33c0b833e585ac910a347
Mikonimo <mikonani413@gmail.com>
2024-04-23 01:23:16 +0300

    task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -55,6 +55,9 @@ class Rectangle(Base):
         """Sets the value of x"""
         if (type(value) is not int):
             raise TypeError("x must be an integer")
+
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -67,6 +70,9 @@ class Rectangle(Base):
         """sets the value of y"""
         if (type(value) is not int):
             raise TypeError("y must be an integer")
+
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):

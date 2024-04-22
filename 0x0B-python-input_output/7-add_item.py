673af76b48425a98230e01c048bd8b8cdb171e0f
Mikonimo <mikonani413@gmail.com>
2024-04-22 21:17:21 +0300

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
 """Contains a script that adds all arguements"""
 import sys
-from os import path
+
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
 
-filename = "add_item.json"
-
-# If file exists, load its content
-if path.exists(filename):
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
-
-# Add command line arguments to the list
-my_list.extend(sys.argv[1:])
-
-# Save the updated list to the file
-save_to_json_file(my_list, filename)
+    try:
+        args = load_from_json_file("add_item.json")
+    except:
+        args = []
+    args.extend(sys.argv[1:])
+    save_to_json_file(args, "add_item.json")

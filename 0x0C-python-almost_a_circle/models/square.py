709ebfd0e39090032e22dd728d008a73f8f55aaf
Mikonimo <mikonani413@gmail.com>
2024-04-25 12:07:57 +0300

    Task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Contains the 'Square class' which  inherits from the Rectangle class"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """This is the Square Class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes the Square Class"""
+        super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+        """String representation of the Square Class"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"

02ad0084dbf91bdef1b3c21adde8080dfc988885
Mikonimo <mikonani413@gmail.com>
2024-04-25 11:27:24 +0300

    Task 8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,15 +90,21 @@ class Rectangle(Base):
                 print("#", end="")
             print()
 
+    def update(self, *args):
+        """Assigns an argument to each attribute"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+
     def __str__(self):
         """String representation of the class"""
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
 {self.__width}/{self.__height}"
-
-    def update(self, *args):
-        """Assigns an argument to each attribute"""
-        self.id = args[1]
-        self.width = args[2]
-        self.height = args[3]
-        self.x = args[4]
-        self.y = args[5]

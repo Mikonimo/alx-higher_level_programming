fc43051893c95b36931b5ccb15e60552e86e8af9
Mikonimo <mikonani413@gmail.com>
2024-04-25 12:11:09 +0300

    Task 10

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initializes the Square Class"""
         super().__init__(size, size, x, y, id)
-    
+
     def __str__(self):
         """String representation of the Square Class"""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.height}"

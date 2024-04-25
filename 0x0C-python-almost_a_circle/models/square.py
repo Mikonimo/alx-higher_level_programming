ff82c851badbdcacd1adcac62dd91a1a8bb9302d
Mikonimo <mikonani413@gmail.com>
2024-04-25 12:45:32 +0300

    Task 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,3 +40,7 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+    
+    def to_dictionary(self):
+        """Returns a dictionary representation of a square"""
+        return {'id': self.id, 'size': self.width, 'x': self.x, 'y': self.y}

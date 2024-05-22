7c27bc80eec946232c965c5722751ab19dcc06f7
Mikonimo <mikonani413@gmail.com>
2024-05-22 18:38:18 +0300

    All tasks

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table second_table in your mysql server
+-- records should be ordered by score(top first)
+-- display score then name(in this order)
+
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- displays the no of records with id = 89 in the table first_table in your mysql server.
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;

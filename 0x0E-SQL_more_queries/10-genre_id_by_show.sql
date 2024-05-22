6d13309168249c2a8fc38e4e78d76aba396aae17
Mikonimo <mikonani413@gmail.com>
2024-05-23 01:37:47 +0300

    Remaining Tasks

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
--- list sll shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- list all shows contained in hbtn_0d_tvshows that have at least one genre linked
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows

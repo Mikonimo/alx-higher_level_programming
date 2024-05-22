fb739528e0507b4146ea35366acd189f86cbd94d
Mikonimo <mikonani413@gmail.com>
2024-05-22 11:13:52 +0300

    Task 0

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,4 @@
+-- This script lists all databases on your MySQL server.
+
+-- Show all databases
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- This script creates the database hbtn_0c_0 in your MySQL server.
+-- If it exists, the script does nothing.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;

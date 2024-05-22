f8e961d759f9cca7ea68fd29513ef4152f26c7b6
Mikonimo <mikonani413@gmail.com>
2024-05-23 00:34:39 +0300

    Task 3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- creates the table force_name on your mysql server
+-- description: id INT, name VARCHAR(256) can't be NULL
+
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);

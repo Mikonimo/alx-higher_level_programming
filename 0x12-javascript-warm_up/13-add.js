71c8711064949a86ef0d3199a0b8500626430020
Mikonimo <mikonani413@gmail.com>
2024-06-17 20:40:02 +0300

    Task 13

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Define the function add
+function add(a, b) {
+    return a + b;
+  }
+
+// Export the add function to make it visible outside the module
+module.exports.add = add;
---
+++ 0x12-javascript-warm_up/13-main.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Import the add function from the module
+const add = require('./13-add').add;
+
+// Use the add function
+console.log(add(3, 5));

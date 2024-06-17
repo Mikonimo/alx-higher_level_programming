8a25a95d7701f68019ef628f1c8f9f74b92c9832
Mikonimo <mikonani413@gmail.com>
2024-06-17 20:24:15 +0300

    Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+// Recursive function to compute factorial
+const factorial = n => {
+    if (n <= 1) {
+      return 1;
+    }
+    return n * factorial(n - 1);
+  };
+
+  const args = process.argv;
+  const num = parseInt(args[2], 10);
+
+  // Compute and print the factorial
+  console.log(factorial(isNaN(num) ? 1 : num));

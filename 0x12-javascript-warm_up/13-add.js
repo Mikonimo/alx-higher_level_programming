df972a269b994bb9de95dfa1569a6bc0163eb4c8
Mikonimo <mikonani413@gmail.com>
2024-06-17 20:41:40 +0300

    Task 13

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 // Define the function add
-function add(a, b) {
-    return a + b;
-  }
+function add (a, b) {
+  return a + b;
+}
 
 // Export the add function to make it visible outside the module
 module.exports.add = add;

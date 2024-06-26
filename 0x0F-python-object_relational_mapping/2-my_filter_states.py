3dabcacc0f5652fcc84de5850b6174778d307e58
Mikonimo <mikonani413@gmail.com>
2024-06-26 12:58:21 +0300

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,8 +16,7 @@ def match_state(username, password, db_name, state_name):
     )
     cursor = db.cursor()
     query = "SELECT id, name FROM state WHERE name = %s ORDER BY id ASC;"
-    query = query.format(state_name)
-    cursor.execute(query)
+    cursor.execute(query, (state_name,))
     results = cursor.fetchall()
 
     for row in results:

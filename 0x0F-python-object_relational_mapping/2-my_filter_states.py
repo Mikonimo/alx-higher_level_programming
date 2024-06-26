8cc3cc46b3968c68858c705b11e1b247a2f13887
Mikonimo <mikonani413@gmail.com>
2024-06-26 12:26:29 +0300

    Task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Takes an argument and displays all values in the states
+table where name matches the argument"""
+import MySQLdb  # type: ignore
+import sys
+
+
+def matching_arg(username, password, db_name, state_name)
+    """Matches the name of the state"""
+    db = MySQLdb.connect(
+        host="localhost",
+        host=3306,
+        user=username,
+        passwd=password,
+        db = db_name
+    )
+    cursor = db.cursor()
+    query = "SELECT * FROM state WHERE name = %s ORDER BY id ASC;"
+    cursor.execute(query, (state_name,))
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    matching_arg(username, password, db_name, state_name)

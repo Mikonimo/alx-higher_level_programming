2c8eed3bdd4b8607331c39247fd65fe1fb34c30a
Mikonimo <mikonani413@gmail.com>
2024-06-27 12:25:19 +0300

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Lists all cities from the database"""
+
 import MySQLdb  # type: ignore
 import sys
-
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
@@ -10,11 +10,12 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        charset="utf8"
     )
     cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, staes.name FROM cities"
-                   "JOIN states ON cities.state_id = states.id"
+    cursor.execute("SELECT cities.id, cities.name, staes.name FROM cities "
+                   "JOIN states ON cities.state_id = states.id "
                    "ORDER BY cities.id ASC")
     results = cursor.fetchall()
     for row in results:

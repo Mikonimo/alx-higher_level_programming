51d97a60cea29b0eac8cfd08dda59aaa0415e4e8
Mikonimo <mikonani413@gmail.com>
2024-06-28 16:22:03 +0300

    task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,9 +9,7 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2], argv[3]),
-        pool_pre_ping=True
-    )
+        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadate.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()

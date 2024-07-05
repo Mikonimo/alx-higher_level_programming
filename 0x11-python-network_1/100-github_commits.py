2a0b8ab0dcc8a237d153929dc0e81c47de850eaa
Mikonimo <mikonani413@gmail.com>
2024-07-05 23:55:54 +0300

    #advanced

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""GitHub API endpoint for listing commits"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = f'https://api.github.com/repos/{owner}/{repository}/commits'
+    params = {
+        'per_page': 10,
+        'page': 1,
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            commits = response.json()
+
+            for commit in commits:
+                sha = commit['sha']
+                author_name = commit['commit']['author']['name']
+                print(f"{sha}: {author_name}")
+        else:
+            print(f"Error: Unable to fetch commits. Status code {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")

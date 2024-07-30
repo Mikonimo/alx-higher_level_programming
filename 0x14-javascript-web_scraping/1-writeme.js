246548ebcb476e6155805dfc8924fa044f5337bf
Mikonimo <mikonani413@gmail.com>
2024-07-30 20:09:14 +0300

    Task 2

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-// Check if both arguments are provided
 // Write the string to the file in UTF-8 encoding
 fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const request = require('request');
+
+// Get the URL from the command line arguments
+const url = process.argv[2];
+
+// Send a GET request to the URL
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    // Print the status code
+    console.log(`code: ${response.statusCode}`);
+  }
+});

0bc34c61f94e39decced7294dec90b535cebfae8
Mikonimo <mikonani413@gmail.com>
2024-07-30 20:58:28 +0300

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,6 +4,9 @@ const request = require('request');
 // Get the API URL from the command line arguments
 const apiUrl = process.argv[2];
 
+// Character ID for Wedge Antilles
+const wedgeAntillesId = 18;
+
 // Send a GET request to the API URL
 request.get(apiUrl, (error, response, body) => {
   if (error) {
@@ -15,7 +18,7 @@ request.get(apiUrl, (error, response, body) => {
     if (data.results) {
       // Filter the movies where Wedge Antilles is present
       const wedgeMovies = data.results.filter(film =>
-        film.characters.includes(`https://swapi-api.alx-tools.com/api/people/18/`)
+        film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)
       );
 
       // Print the number of movies
